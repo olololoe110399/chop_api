b8ae90c1ced5e44fda55335ca7c1d553896379a3
Duy Nguyen Ngoc <duynn100198@gmail.com>
2023-11-26 03:24:21 +0700

    feature: integrate gpt 4

--- app/Demo.py
+++ app/Demo.py
@@ -2,21 +2,22 @@ import os
 
 import streamlit as st
 from langchain.document_loaders import TextLoader
-from init.llm import LLM
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+from app.init.llm2 import Recommender
 from init import check_an_create_folder, handle_json_file
-from init.print_retrieval_handler import PrintRetrievalHandler
-from init.stream_handler import StreamHandler
 
 st.header(":rocket: Trò chuyện với ChopAI :page_facing_up:")
 st.write("Định dạng file hỗ trợ: JSON :city_sunrise:")
 
-llm = LLM()
+# llm = LLM()
 
 with st.sidebar.expander("Settings", expanded=True):
     uploaded_file = st.file_uploader("Vui lòng tải lên danh sách sản phẩm của bạn", accept_multiple_files=False,
                                      type=None)
-    if uploaded_file:
-        with st.spinner('Đang tải đang dữ liệu...'):
+    if uploaded_file and "vectorstore" not in st.session_state and "client" not in st.session_state:
+        with st.spinner('Đang tải dữ liệu...'):
             file_path = os.path.join(
                 os.getcwd(), "app/resources", uploaded_file.name
             )
@@ -28,30 +29,43 @@ with st.sidebar.expander("Settings", expanded=True):
                     file_path="app/resources/data_processed.txt",
                     encoding="utf-8",
                 )
+                from langchain.text_splitter import RecursiveCharacterTextSplitter
+                data = loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter(
+                    chunk_size=1000,
+                    chunk_overlap=0,
+                ))
+                embeddings = OpenAIEmbeddings()
+                vectorstore = FAISS.from_documents(data, embeddings)
 
-                data = loader.load()
-                conversation_retrieval_chain = llm.init_conversation_retrieval_chain(data)
-                # conversation_chain = llm.init_conversation_chain()
-                st.session_state['conversation_retrieval_chain'] = conversation_retrieval_chain
+                st.session_state['vectorstore'] = vectorstore
+                st.session_state['recommender'] = Recommender()
                 st.success(f"Đã xử lý xong file {uploaded_file.name}")
 
 if not uploaded_file:
     st.info("Vui lòng tải lên danh sách sản phẩm của bạn để tiếp tục.", icon="🚨")
     st.stop()
 
-if not data:
-    st.info("Đã có lỗi xảy ra, vui lòng thử lại.", icon="🚨")
-    st.stop()
 
-if "conversation_retrieval_chain" in st.session_state:
-    conversation_retrieval_chain = st.session_state['conversation_retrieval_chain']
+def markdown_realtime(text):
+    st.markdown(text)
+
+
+if "recommender" in st.session_state:
+    vectorstore = st.session_state['vectorstore']
+    recommender = st.session_state['recommender']
 
     if "messages" not in st.session_state:
-        st.session_state.messages = []
+        with open("app/resources/system_message", "r") as file:
+            system_message = file.read()
+        st.session_state.messages = [{
+            "role": "system",
+            "content": system_message
+        }]
 
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        if message["role"] != "system":
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
     if prompt := st.chat_input("Ask your questions?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
@@ -59,13 +73,57 @@ if "conversation_retrieval_chain" in st.session_state:
             st.markdown(prompt)
 
         with st.chat_message("assistant"):
-            retrieval_handler = PrintRetrievalHandler(st.container())
-            stream_handler = StreamHandler(st.empty())
-            result = conversation_retrieval_chain({
-                "question": prompt,
-                "chat_history": [
-                    (message["role"], message["content"]) for message in
-                    st.session_state.messages
-                ],
-            }, callbacks=[retrieval_handler, stream_handler])
-        st.session_state.messages.append({"role": "assistant", "content": result["answer"]})
+            try:
+                with st.spinner('Đang suy nghĩ  ...'):
+                    recommender_response = recommender.recommend("user", prompt)
+                if 'search term for retrieval' not in recommender_response:
+                    st.session_state.messages.append(
+                        {"role": "assistant",
+                         "content": recommender_response['your response to user']}
+                    )
+                    st.markdown(recommender_response['your response to user'])
+                    st.stop()
+
+                if len(recommender_response['search term for retrieval']) == 0:
+                    st.session_state.messages.append({"role": "assistant",
+                                                      "content": recommender_response['your response to user']}
+                                                     )
+                    st.markdown(recommender_response['your response to user'])
+                    st.stop()
+
+                results = vectorstore.similarity_search(
+                    ", ".join(recommender_response['search term for retrieval']))
+                if len(results) == 0:
+                    st.session_state.messages.append({
+                        "role": "assistant",
+                        "content": recommender_response['your response to user']
+                    })
+                    st.markdown(recommender_response['your response to user'])
+                    st.stop()
+                retrievals = []
+                for result in results[:3]:
+                    retrievals.append(result.page_content)
+                if len(retrievals) == 0:
+                    st.session_state.messages.append({
+                        "role": "assistant",
+                        "content": recommender_response['your response to user']
+                    })
+                    st.markdown(recommender_response['your response to user'])
+                    st.stop()
+                with st.spinner('Đang gợi ý  ...'):
+                    final_result = recommender.recommend(
+                        "assistant",
+                        f"Đây là trang phục phù hợp với người dùng hãy:\n\n {retrievals}"
+                        f"hãy ghi nhớ những đề xuất này trong suốt cuộc trò chuyện "
+                        f"của bạn với người dùng và bây giờ hãy gợi ý sản phẩm phù hợp hơn cho người dùng. "
+                        f"(Luôn luôn đính kèm sản phẩm)",
+                    )
+                    st.markdown(final_result['your response to user'])
+                    st.session_state.messages.append(
+                        {"role": "assistant",
+                         "content": final_result['your response to user']}
+                    )
+            except Exception as e:
+                print(e)
+                st.error("Có lỗi xảy ra, vui lòng thử lại.")
+                st.stop()
